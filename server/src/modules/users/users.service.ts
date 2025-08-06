fd5ea4e0860f92001cb97cf0b6a3db14da738234
Arturstriker3 <artur.danielgama@gmail.com>
2025-08-06 04:45:44 -0300

    🦄 refactor: remove unused getUserByToken method from UsersService

--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -18,30 +18,4 @@ export class UsersService {
   async getUserById(userId: string): Promise<UserProfileDto> {
     return await this.getUserByIdUseCase.execute(userId);
   }
-
-  async getUserByToken(token: string): Promise<UserProfileDto> {
-    // Verifica e decodifica o token
-    const tokenPayload = AuthMiddleware.verifyToken(token);
-    if (!tokenPayload) {
-      throw new Error("Token inválido ou expirado");
-    }
-
-    // Busca o usuário pelo ID do token
-    const user = await this.usersRepository.findById(tokenPayload.userId);
-
-    // Valida se o usuário existe
-    UserBusinessRules.validateUserForLogin(user);
-
-    const validUser = user!;
-
-    // Retorna os dados do usuário sem informações sensíveis
-    return new UserProfileDto({
-      id: validUser.id,
-      nome: validUser.nome,
-      email: validUser.login,
-      role: validUser.nivel_acesso,
-      criado_em: validUser.criado_em,
-      alterado_em: validUser.alterado_em,
-    });
-  }
 }
